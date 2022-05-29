68b73997309d323802a0758c3042c6917ccf5119
HagerHany <hanyhager120@gmail.com>
2022-05-29 16:51:03 +0200

    adding CSVReader Feature

--- src/test/java/tests/TestRegistrationCSV.java
+++ src/test/java/tests/TestRegistrationCSV.java
@@ -2,11 +2,13 @@ package tests;
 
 import Pages.RegistrationPage;
 import Utilities.Helper;
-import data.ExcelReader;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import java.io.FileReader;
 import java.io.IOException;
 
 public class TestRegistrationCSV {
@@ -18,21 +20,31 @@ public class TestRegistrationCSV {
         RegPage= new RegistrationPage(this.getClass().getName(), browser);
         RegPage.getLink("https://demo.nopcommerce.com/register?returnUrl=%2F");
     }
-    @DataProvider(name = "userInfo")
-    public static Object[][] userInfo() throws IOException {
-        //read from excel
-        ExcelReader reader =new ExcelReader();
-        return reader.getExcelData();
 
-    }
 
-    @Test(dataProvider="userInfo")
-    public void Register(String gender, String fName, String lName, String email, String userPass) throws InterruptedException {
-        RegPage.enterInfo(gender, fName, lName, email, userPass);
-        RegPage.ClickOnRegBtn();
-        Assert.assertEquals("Your registration completed",RegPage.getTextLogin());
-        RegPage.logOut();
-        RegPage.getLink("https://demo.nopcommerce.com/register?returnUrl=%2F");
+    @Test()
+    public void Register() throws CsvValidationException, IOException, InterruptedException {
+        CSVReader reader;
+        String csvFile= System.getProperty("user.dir")+"\\src\\test\\java\\data\\UserInfo.csv";
+        reader = new CSVReader(new FileReader(csvFile));
+        String []csvCell;
+        while ((csvCell= reader.readNext())!=null)
+        {
+            String gender= csvCell[0];
+            String fName= csvCell[1];
+            String lName= csvCell[2];
+            String email=csvCell[3];
+            String userPass= csvCell[4];
+            RegPage.enterInfo(gender, fName, lName, email, userPass);
+            RegPage.ClickOnRegBtn();
+            Assert.assertEquals("Your registration completed",RegPage.getTextLogin());
+            RegPage.logOut();
+            Thread.sleep(5000);
+            RegPage.getLink("https://demo.nopcommerce.com/register?returnUrl=%2F");
+            Thread.sleep(5000);
+        }
+
+
     }
 
     @AfterMethod
